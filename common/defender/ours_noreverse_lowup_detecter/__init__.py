6c80f15a9050287260253b51d60c7ffaafdae646
RetrievalBackdoorDefense <1562944681@qq.com>
2025-02-15 06:39:41 +0000

    Delete useless defenders

--- common/defender/ours_noreverse_lowup_detecter/__init__.py
+++
@@ -1,1 +0,0 @@
-from .defender import OursNoReverseLowUpDetecterDefender
--- common/defender/ours_openstop_lowup_detecter/detecter/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gmm import GMMDetecter
-from .bert import BERTDetecter
-from .lstm import LSTMDetecter
-from .rbert import RBERTDetecter
