50efb1f695fda81550aeebe875bb50ed2f8185b6
RetrievalBackdoorDefense <1562944681@qq.com>
2025-02-15 07:21:46 +0000

    add pretrained-model

---
+++ common/pretrained-model/download.sh
@@ -0,0 +1,5 @@
+modelscope download --model AI-ModelScope/bert-base-uncased --local_dir ./bert-base-uncased
+
+modelscope download --model BAAI/bge-large-en-v1.5 --local_dir ./bge-large-en-v1.5
+
+modelscope download --model cjc1887415157/UAE-Large-V1 --local_dir ./uae-large-v1
